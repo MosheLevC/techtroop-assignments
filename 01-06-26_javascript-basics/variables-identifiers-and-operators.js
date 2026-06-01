0b9909fa1840c2da87ac7418cc2be64dcd2c494f
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-01 13:38:08 +0300

    finished exercise 1: basic variable and operator examples

---
+++ 01-06-26_javascript-basics/variables-identifiers-and-operators.js
@@ -0,0 +1,11 @@
+const exercise1 = () => {
+(5 > 2) && false // false
+!("knife" === "sword") // true
+(1 < 2) || (-1 > -1) || !false // true
+"" // false
+(31 % 5) == "1" // true
+!!true //true
+"5th Avenue" != "5th Avenue" // false
+52 !== "52" //true
+(undefined || null) //false
+}
