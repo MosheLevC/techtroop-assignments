d22367b9307fc166adc49821332c6728f1217ce0
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-07 17:48:12 +0300

    fix: correct 'this' reference in revealSecret and fix console log syntax

--- 02-06-26_js-this/Ex5.js
+++ 02-06-26_js-this/Ex5.js
@@ -1,21 +1,21 @@
 const revealSecret = function () {
-  return secret;
+  return this.secret;
 };
 
 const shoutIt = function (person, func) {
   person.revealItAll = func;
   const result = person.revealItAll();
-  console.log(person.name + " said: " result);
+  console.log(person.name + " said: " + result);
 };
 
 const avi = {
-  name: "Avi"
-  secret: "Im scared of snakes!"
+  name: "Avi",
+  secret: "Im scared of snakes!",
 };
 
 const narkis = {
-  name: "Narkis"
-  secret: "I don't have secrets because I'm zen like that."
+  name: "Narkis",
+  secret: "I don't have secrets because I'm zen like that.",
 };
 
 shoutIt(avi, revealSecret);
