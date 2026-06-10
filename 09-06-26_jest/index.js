e2a6dd0072f8a7d4f6cbef55728202e140c853ba
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-10 21:13:41 +0300

    Ex2: add removeAtLeastOne function and corresponding tests for array item removal

--- 09-06-26_jest/index.js
+++ 09-06-26_jest/index.js
@@ -5,4 +5,12 @@ const isEven = (n) => {
   return n % 2 == 0 ? true : false;
 };
 
-module.exports = { isEven };
+//Ex2
+//should remove at least one element from the array `arr`
+const removeAtLeastOne = (arr) => {
+  let numItemsToRemove = Math.floor(Math.random() * (arr.length - 1)) + 1;
+  arr.splice(0, numItemsToRemove);
+  return arr;
+};
+
+module.exports = { isEven, removeAtLeastOne };
--- 09-06-26_jest/index.test.js
+++ 09-06-26_jest/index.test.js
@@ -1,8 +1,15 @@
-const { isEven } = require(".");
+const { isEven, removeAtLeastOne } = require(".");
 
+//Ex1
 test("should return boolean of if it's even", () => {
   const odd = isEven(3);
   const even = isEven(8);
   expect(odd).toBeFalsy();
   expect(even).toBeTruthy();
+});
+
+//Ex2
+test("should contain less items than original array", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  expect(removeAtLeastOne([...arr]).length).toBeLessThan(arr.length);
 });
