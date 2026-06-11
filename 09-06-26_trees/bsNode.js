02d5f34762f03c30b58cfef3a5ab202da725afa4
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-11 12:54:34 +0300

    Ex1: add findNode function to search for values in the binary search tree

--- 09-06-26_trees/bsNode.js
+++ 09-06-26_trees/bsNode.js
@@ -5,7 +5,7 @@ const createBSNode = (value) => ({
 });
 
 const insertNode = (node, newVal) => {
-  if (!node.value) {
+  if (node.value === undefined || node.value === null) {
     node.value = newVal;
   } else if (newVal > node.value && node.rightChild) {
     insertNode(node.rightChild, newVal);
@@ -16,6 +16,23 @@ const insertNode = (node, newVal) => {
   } else {
     node.rightChild = createBSNode(newVal);
   }
+};
+
+const findNode = (node, searchedValue) => {
+  if (node.value === undefined || node.value === null) {
+    console.log(false);
+    return false;
+  }
+  if (node.value === searchedValue) {
+    console.log(true);
+    return true;
+  } else if (searchedValue > node.value && node.rightChild) {
+    return findNode(node.rightChild, searchedValue);
+  } else if (searchedValue <= node.value && node.leftChild) {
+    return findNode(node.leftChild, searchedValue);
+  }
+  console.log(false);
+  return false;
 };
 
 module.exports = {
--- 09-06-26_trees/index.js
+++ 09-06-26_trees/index.js
@@ -1,8 +1,15 @@
-const { createBSNode, insertNode } = require("./bsNode");
+const { createBSNode, insertNode, findNode } = require("./bsNode");
 
 const myTree = createBSNode();
 const nodeValues = ["H", "E", "S", "G", "L", "Y", "I"];
+
 for (const value of nodeValues) {
   insertNode(myTree, value);
 }
-console.log(myTree);
+
+// Ex1
+findNode(myTree, "H"); // should print true
+findNode(myTree, "G"); // should print true
+findNode(myTree, "Z"); // should print false
+findNode(myTree, "F"); // should print false
+findNode(myTree, "y"); // should print false - we didn't make the tree case sensitive!
