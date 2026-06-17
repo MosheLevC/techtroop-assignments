286af8eb108249938df5f2a1e85d72a5ab0ac220
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-18 01:24:31 +0300

    finished Ex5

--- 16-06-26-ajax-and-api/Ex5/main.js
+++ 16-06-26-ajax-and-api/Ex5/main.js
@@ -1,20 +1,23 @@
 import axios from "../../node_modules/axios/dist/esm/axios.js";
 import { GIPHY_API_KEY } from "./config.js";
 
-const catGif = () => {
+const searchButton = document.getElementById("search-button");
+const searchbox = document.getElementById("searchbox");
+const iFrame = document.createElement("iframe");
+
+const catGif = (input) => {
   axios
-    .get(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=${GIPHY_API_KEY}&limit=1`)
+    .get(`https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${GIPHY_API_KEY}&limit=1`)
     .then(({ data }) => {
       const body = document.getElementById("body");
       const gifData = data?.data?.[0];
 
       if (gifData?.embed_url) {
-        const iFrame = document.createElement("iframe");
         iFrame.src = gifData.embed_url;
         body.appendChild(iFrame);
       } else {
         const text = document.createElement("p");
-        text.textContent = "cats not found";
+        text.textContent = "gif not found";
         body.appendChild(text);
       }
     })
@@ -22,4 +25,7 @@ const catGif = () => {
       console.log(error);
     });
 };
-catGif();
+
+searchButton.addEventListener("click", () => {
+  catGif(searchbox.value);
+});
