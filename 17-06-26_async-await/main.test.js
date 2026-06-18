4ce0f41c49dee3dd328f9f89a5d7aba470064d9e
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-18 03:34:54 +0300

    seperate into 2 tests

--- 17-06-26_async-await/main.test.js
+++ 17-06-26_async-await/main.test.js
@@ -4,5 +4,9 @@ test("should return user by ID", async () => {
   const valid = await getUserById(2);
   const invalid = await getUserById(999);
   expect(valid.name).toBe("Ervin Howell");
+});
+
+test("should return null if user doesn't exist", async () => {
+  const invalid = await getUserById(999);
   expect(invalid).toBeNull();
 });
